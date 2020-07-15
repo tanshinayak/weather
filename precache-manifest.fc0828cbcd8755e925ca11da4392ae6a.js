9c8d070ee4f80924fe41cecac735b453b5883097
Tanshi Nayak <tanshitn@gmail.com>
2020-07-15 20:43:36 +0530

    Updates

---
+++ precache-manifest.fc0828cbcd8755e925ca11da4392ae6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880660c1d78a1e6dea2c59f2a423318c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56604174c117bfb4f8a0",
+    "url": "/static/css/main.50e4ad36.chunk.css"
+  },
+  {
+    "revision": "7494add9c60ab7c9cce4",
+    "url": "/static/js/2.d4039f96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4039f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56604174c117bfb4f8a0",
+    "url": "/static/js/main.5b74d7c8.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "78907658f3b369f2b6ed5d4fb9ae48d2",
+    "url": "/static/media/background.78907658.jpg"
+  },
+  {
+    "revision": "a9e8c39738d4a44530cb482d05e704a6",
+    "url": "/static/media/github.a9e8c397.png"
+  }
+]);
